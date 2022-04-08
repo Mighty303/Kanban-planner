042bdf91a17f4f65be2ed43078b47777626cb8c3
Martin <mwong179@mylangara.ca>
2022-04-07 22:27:25 -0700

    deploy

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const connection = require('./db/connection');
 
+const server = app.listen(process.env.PORT || 8080, ()=>console.log("Listening on 8080"));
 connection.once('open', ()=> {
-    const server = app.listen(process.env.PORT || 8080, ()=>console.log("Listening on 8080"));
 });
 
 app.use(express.static("public"));
@@ -16,3 +16,7 @@ app.get('/api/v1/tasks', (req, res)=> {
 app.post('/api/v1/tasks', (req, res)=> {
     res.send('success');
 });
+
+app.get('*', (req, res) =>{
+    
+})
