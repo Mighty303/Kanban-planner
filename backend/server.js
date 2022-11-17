2c3dabf98c1f3bc5f8fbc4068bbaac3956075c59
Mighty303 <martinwong303@gmail.com>
2022-11-17 10:07:06 -0800

    renamed folders

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-const connection = require('./db/connection');
+const connection = require('./database/connection');
 const path = require('path');
 
 app.listen(process.env.PORT || 8080, ()=>console.log("Listening on 8080"));
